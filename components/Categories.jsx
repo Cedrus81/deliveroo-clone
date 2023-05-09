6d669658715f628ea070bf9d210dfd41e1ff463f
Cedrus81 <erez.eitan.97@gmail.com>
2023-05-09 16:42:40 +0300

    made category row cmp, now working on featured row

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
+import CategoryCard from './CategoryCard'
 
 const Categories = () => {
     return (
-        <View>
-            <Text>Categories</Text>
-        </View>
+        <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{
+                paddingHorizontal: 15,
+                paddingTop: 10
+            }}>
+            <CategoryCard imgUrl='https://links.papareact.com/gn7' title="testing" />
+            <CategoryCard imgUrl='https://links.papareact.com/gn7' title="testing" />
+            <CategoryCard imgUrl='https://links.papareact.com/gn7' title="testing" />
+            <CategoryCard imgUrl='https://links.papareact.com/gn7' title="testing" />
+            <CategoryCard imgUrl='https://links.papareact.com/gn7' title="testing" />
+            <CategoryCard imgUrl='https://links.papareact.com/gn7' title="testing" />
+            <CategoryCard imgUrl='https://links.papareact.com/gn7' title="testing" />
+            <CategoryCard imgUrl='https://links.papareact.com/gn7' title="testing" />
+            <CategoryCard imgUrl='https://links.papareact.com/gn7' title="testing" />
+        </ScrollView>
     )
 }
 
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -4,7 +4,8 @@ import { useNavigation } from '@react-navigation/native'
 import { UserIcon, ChevronDownIcon, MagnifyingGlassIcon, AdjustmentsVerticalIcon } from 'react-native-heroicons/solid'
 
 import Categories from '../components/Categories'
-
+import FeaturedRow from '../components/FeaturedRow'
+//! stopped at 59:41 in the video, at the begginning of FeaturedRow.jsx
 const HomeScreen = () => {
     const navigation = useNavigation()
     const styles = {
@@ -20,34 +21,39 @@ const HomeScreen = () => {
     }, [])
     return (
         <SafeAreaView className="bg-white" style={{ paddingTop: StatusBar.currentHeight }}>
-            <Text>
-                {/* Header */}
-                <View className="flex-row w-screen pb-3 items-center mx-4 space-x-2 px-4">
-                    <Image source={{ uri: 'https://links.papareact.com/wru' }} className='h-7 w-7 bg-gray-300 p-4 rounded-full' />
-                    <View className="flex-1">
-                        <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-                        <View className="flex-row items-center">
-                            <Text className="font-bold text-xl">Current Location</Text>
-                            <ChevronDownIcon size={20} color='#00ccbb' style={styles.svg} />
-                        </View>
+            {/* Header */}
+            <View className="flex-row pb-3 items-center mx-4 space-x-2">
+                <Image source={{ uri: 'https://links.papareact.com/wru' }} className='h-7 w-7 bg-gray-300 p-4 rounded-full' />
+                <View className="flex-1">
+                    <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+                    <View className="flex-row items-center">
+                        <Text className="font-bold text-xl">Current Location</Text>
+                        <ChevronDownIcon size={20} color='#00ccbb' style={styles.svg} />
                     </View>
-                    <UserIcon size={35} color='#00ccbb' style={styles.svg} />
                 </View>
-                {/* Search */}
-                <View className="flex-row w-screen items-center space-x-2 pb-2 mx-4 px-4">
-                    <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
-                        <MagnifyingGlassIcon color='gray' size={20} style={styles.svg} />
-                        <TextInput placeholder="Restaurants and cuisines" keyboardType='default' />
-                    </View>
-                    <AdjustmentsVerticalIcon color='#00ccbb' style={styles.svg} />
+                <UserIcon size={35} color='#00ccbb' style={styles.svg} />
+            </View>
+            {/* Search */}
+            <View className="flex-row items-center space-x-2 pb-2 mx-4">
+                <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
+                    <MagnifyingGlassIcon color='gray' size={20} style={styles.svg} />
+                    <TextInput placeholder="Restaurants and cuisines" keyboardType='default' />
                 </View>
-                {/* Body */}
-                <ScrollView>
-                    {/* Categories */}
-                    <Categories />
-                    {/* Featured Rows */}
-                </ScrollView>
-            </Text>
+                <AdjustmentsVerticalIcon color='#00ccbb' style={styles.svg} />
+            </View>
+            {/* Body */}
+            <ScrollView className="bg-gray-100" contentContainerStyle={{
+                paddingBottom: 100
+            }}>
+                {/* Categories */}
+                <Categories />
+                {/* Featured Rows */}
+                <FeaturedRow
+                    title="Featured"
+                    description="paid placements from our partners"
+                    featuredCategory="featured"
+                />
+            </ScrollView>
         </SafeAreaView>
     )
 }
